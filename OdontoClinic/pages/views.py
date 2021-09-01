5b48ccf1a14a693331435ba398690e51535e5032
Viking-Code-SpecOps <rafaeel5005@gmail.com>
2021-09-01 02:30:54 -0300

    base and home created(raw)

---
+++ OdontoClinic/pages/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from datetime import datetime
+
+now = datetime.now().strftime('%H:%M:%S')
+
+def HomePage(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    context = {
+            "actual_time": now
+        }
+    return render(request, "base.html", context)
