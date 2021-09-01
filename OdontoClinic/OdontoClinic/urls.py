dfb86130f5ea3e0f7d4417370e3784ce03932c39
Viking-Code-SpecOps <rafaeel5005@gmail.com>
2021-09-01 20:28:41 -0300

    base page almost ready

--- OdontoClinic/OdontoClinic/urls.py
+++ OdontoClinic/OdontoClinic/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from pages.views import HomePage
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", HomePage, name="home")
