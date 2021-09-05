c07ee0fcfcfb46464ed12306e9c150a509e1a5a8
Viking-Code-SpecOps <rafaeel5005@gmail.com>
2021-09-05 01:23:15 -0300

    back-end of scheduling made(part one)

---
+++ OdontoClinic/Agenda/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Agendamento(models.Model):
+    id           = models.AutoField(auto_created=True, primary_key=True)
+    PrimeiroNome = models.CharField(max_length=50, blank=False)
+    Sobrenome    = models.CharField(max_length=50, blank=False)
+    Telefone     = models.CharField(max_length=50, default="(00)00000-0000", blank=False)
+    Dor          = models.BooleanField(default=False)
+    Problema     = models.TextField(default="descreva o que sente", blank=False)
+
+
+
